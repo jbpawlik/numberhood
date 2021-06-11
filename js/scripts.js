0b849b6b83263203dd102b3a89b5ad002f2ef326
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-11 09:29:14 -0700

    WIP: loop through input to find 1s

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,60 @@
 //Business Logic
 
+
+
+
 function numberhood(number) {
   let numberInput = [parseInt(number)];
-  if (numberInput === 1) {
+  for (i = 0; i < numberInput.length; i++) {
+    let a = numberInput[i].includes(1);
+    if (a = true) {
     return 'Beep!';
-  }
-} numberhood(1)
+  } else {
+    return numberInput
+  }}
+} numberhood(11)
+
+
+// function numberhood(number) {
+//   let numberInput = [parseInt(number)];
+//   let checkfor1 = numberInput.includes(1);
+//   for (i = 0; i <= numberInput.length; i++) {
+//     if (checkfor1 === true) {
+//     return 'Beep!';
+//   } else { 
+//     return numberInput
+//   }}
+// } numberhood(11)
+
+
+
+// function numberhood(number) {
+//   let numberInput = [parseInt(number)];
+//   for (i = 0; i <= numberInput.length; i++) {
+//   if (numberInput[0] === 1) {
+//     return 'Beep!';
+//   } else { 
+//     return numberInput
+//   }}
+// } numberhood(11)
+
+// function numberhood(number) {
+//   let numberInput = [parseInt(number)];
+  
+  
+//   if (numberInput === 1) {
+//     return 'Beep!';
+//   } else {
+//     return numberInput
+//   }
+// } numberhood(1)
+
+
+// function numberhood(number) {
+//   let numberInput = parseInt(number);
+//   if (numberInput === 1) {
+//     return 'Beep!';
+//   } else {
+//     return numberInput
+//   }
+// } numberhood(1)
