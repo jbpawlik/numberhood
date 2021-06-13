e9e0e992824041a8e0e841abd9c53dc3932a0d50
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-13 14:08:39 -0700

    BUGFIX: fix NaN bug by removing all parseInt functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 //Business Logic
 
 function notANumber(number) {
-  let inputNumber = parseInt(number);
-  if (isNaN(inputNumber) === false) {
+  if (isNaN(number) === false) {
     return true;
   } else {
     return false;
@@ -12,16 +11,35 @@ function notANumber(number) {
 function numberhood(number) {
   let numberInput = number.toString()
   numberArray = [numberInput.split("")]
+  if (notANumber(number) === true) {
     for (i = 0; i < numberArray.length; i+=1) {
-    if (notANumber(number) === true && numberInput.includes(3)) {
-      return 'includes3';
-    } else if (notANumber(number) === true && numberInput.includes(2)) {
-      return 'includes2';
-    } else if (notANumber(number) === true && numberInput.includes(1)) {
-      return 'includes1';
+      if (numberInput.includes(3)) {
+        return 'includes3';
+      } else if (numberInput.includes(2)) {
+        return 'includes2';
+      } else if (numberInput.includes(1)) {
+        return 'includes1';
+      } 
     }
   }
 }
+
+
+
+
+// function numberhood(number) {
+//   let numberInput = number.toString()
+//   numberArray = [numberInput.split("")]
+//     for (i = 0; i < numberArray.length; i+=1) {
+//     if (notANumber(number) === true && numberInput.includes(3)) {
+//       return 'includes3';
+//     } else if (notANumber(number) === true && numberInput.includes(2)) {
+//       return 'includes2';
+//     } else if (notANumber(number) === true && numberInput.includes(1)) {
+//       return 'includes1';
+//     }
+//   }
+// }
 
 
 // function numberhood(number) {
@@ -99,20 +117,16 @@ $(document).ready(function() {
     $("#outputField").show();
 
     const userNumber = $("#userNumber").val();
-    let result = parseInt(userNumber)
+    let result = userNumber
 
     if (notANumber(userNumber) === false) {
-      $("#output").text('ERROR! ERROR! ERROR!  DOES NOT COMPUTE; USER DID NOT ENTER NUMBER; PRINT: "IT IS A TERRIBLE DAY IN THE NEIGHBORHOOD :("  WILL SELF DESTRUCT IF NO NUMBER ENTERED! 10... 9... 8...')
-    } else if (notANumber(userNumber) === true) {
-    $("#output").text('COMPUTING... COMPUTING...COMPUTING...')
-    }  
-
-    if (numberhood(userNumber) === 'includes3') {
+      $("#numberOutput").text('ERROR! ERROR! ERROR!  DOES NOT COMPUTE; USER DID NOT ENTER NUMBER; PRINT: "IT IS A TERRIBLE DAY IN THE NEIGHBORHOOD :("  WILL SELF DESTRUCT IF NO NUMBER ENTERED! 10... 9... 8...')
+    } else if (numberhood(userNumber) === 'includes3') {
       $("#numberOutput").text(result + "!!! MY FAVORITE NUMBER. WON'T YOU BE MY NEIGHBOR?")
     } else if (numberhood(userNumber) === 'includes2') {
-      $("#numberOutput").text(result + "!!! BOOP!")
+      $("#numberOutput").text(result + "!!! BOOP! BOOP! BOOP!")
     } else if (numberhood(userNumber) === 'includes1') {
-    $("#numberOutput").text(result + "!!! BEEP!")
+    $("#numberOutput").text(result + "!!! BEEP! BEEP! BEEP!")
     } else 
     $("#numberOutput").text(result + "!!! GREAT NUMBER!")
   })
