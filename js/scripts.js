0676f41e5e754f39133d50f62cecbefe53c2c78e
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-13 16:06:27 -0700

    WIP: add loop to compute viral load based on user input

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,8 @@ function notANumber(number) {
 } 
 
 function numberhood(number) {
-  let numberInput = number.toString()
-  numberArray = [numberInput.split("")]
+  let numberInput = number.toString();
+  const numberArray = [numberInput.split("")];
   if (notANumber(number) === true) {
     for (i = 0; i < numberArray.length; i+=1) {
       if (numberInput.includes(3)) {
@@ -19,11 +19,11 @@ function numberhood(number) {
         return 'includes2';
       } else if (numberInput.includes(1)) {
         return 'includes1';
-      } 
+      } else return number;
     }
   }
 }
-
+  
 
 
 
@@ -122,12 +122,28 @@ $(document).ready(function() {
     if (notANumber(userNumber) === false) {
       $("#numberOutput").text('ERROR! ERROR! ERROR!  DOES NOT COMPUTE; USER DID NOT ENTER NUMBER; PRINT: "IT IS A TERRIBLE DAY IN THE NEIGHBORHOOD :("  WILL SELF DESTRUCT IF NO NUMBER ENTERED! 10... 9... 8...')
     } else if (numberhood(userNumber) === 'includes3') {
-      $("#numberOutput").text(result + "!!! MY FAVORITE NUMBER. WON'T YOU BE MY NEIGHBOR?")
+      $("#numberOutput").text(result + "!!! MY FAVORITE NUMBER!!! WON'T YOU BE MY NEIGHBOR???")
     } else if (numberhood(userNumber) === 'includes2') {
-      $("#numberOutput").text(result + "!!! BOOP! BOOP! BOOP!")
+      $("#numberOutput").text(result + "!!! BOOP!!! BOOP!!! BOOP!!!")
     } else if (numberhood(userNumber) === 'includes1') {
     $("#numberOutput").text(result + "!!! BEEP! BEEP! BEEP!")
     } else 
     $("#numberOutput").text(result + "!!! GREAT NUMBER!")
+
+    $('#virusOutput').text(result)
   })
+
+  $('button#virusButton').click(function(event) {
+    event.preventDefault();
+
+    const userNumber = $('#userNumber').val();
+
+    if (notANumber(userNumber) === false) {
+      alert('ERROR: NOT A NUMBER; VIRUS DEFEATED')
+    } else {
+    alert('You won')
+    alert('10000000')
+    alert("viruses!!!!!");
+    }
+  });
 });
