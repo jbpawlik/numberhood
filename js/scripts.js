3f42e64bf9213f6eeb4c5b2606a8673b622a17fc
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-13 11:48:55 -0700

    add function to return 'Boop' if number is two (Spec 5)

--- js/scripts.js
+++ js/scripts.js
@@ -7,203 +7,53 @@ function notANumber(number) {
   } else {
     return false;
   }
-} notANumber(1)
+} 
 
-function number1(number) {
-  let numberInput = number.toString()
-  numberArray = [numberInput.split("")]
-    for (i = 0; i < numberArray.length; i+=1) {
-    if (numberInput.includes(2)) {
-    return 'Boop!';
-  } else {
-    return numberInput
-  }}
-} number1(111)
-
-// Not a number function (working)
-// function notANumber(number) {
-//   let inputNumber = parseInt(number);
-//   if (isNaN(inputNumber) === false) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// } notANumber(1)
-
-// function notANumber(number) {
-//   let inputNumber = parseInt(number);
-//   if (inputNumber === Number) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// } notANumber(1)
-
-// function notANumber(number) {
-//   if (parseInt(number) !== Number) {
-//     alert('Please enter a number')
-//   } else {
-//     return isANumber = parseInt(number)
-//   }
-// } notANumber('a')
-
-
-function number1(number) {
-  let numberInput = number.toString()
-  numberArray = [numberInput.split("")]
-    for (i = 0; i < numberArray.length; i+=1) {
-    if (numberInput.includes(2)) {
-    return 'Boop!';
-  } else {
-    return numberInput
-  }}
-} number1(111)
-
-function number2(number) {
+function numberhood(number) {
   let numberInput = number.toString()
   numberArray = [numberInput.split("")]
     for (i = 0; i < numberArray.length; i+=1) {
     if (numberInput.includes(1)) {
-    return 'Beep!';
+    return 'Boop!';
   } else {
     return numberInput
   }}
-} number2(111)
+}
 
-function number3(number) {
-  let numberInput = number.toString()
-  numberArray = [numberInput.split("")]
-    for (i = 0; i < numberArray.length; i+=1) {
-    if (numberInput.includes(3)) {
-    return "Won't you be my neighbor?";
-  } else {
-    return numberInput
-  }}
-} number3(111)
-
-// function numberhood(number) {
-//   let numberInput = '111'
+// function number1(number) {
+//   let numberInput = number.toString()
 //   numberArray = [numberInput.split("")]
-// } 
+//     for (i = 0; i < numberArray.length; i+=1) {
+//     if (numberInput.includes(2)) {
+//     return 'Boop!';
+//   } else {
+//     return numberInput
+//   }}
+// } number1(111)
 
-
-
-// function numberhood(number) {
-//   const numberInput = [parseInt(number)]
-//   let checkFor1 = false;
-//   numberInput.forEach(function(element) {
-//     if (numberInput.includes(1) === true) {
-//       checkFor1 = true;
-//     } else {
-//       checkFor1 = false
-//     }
-//     console.log(checkFor1)
-//   })
-// } numberhood(11)
-
-
-
-// function numberhood(number) {
-//   const numberInput = [parseInt(number)]
-//   let checkFor1 = false;
-//   numberInput.forEach(function(element) {
-//     if (numberInput.includes(1) === true) {
-//       checkFor1 = true;
-//     } else {
-//       checkFor1 = false
-//     }
-//     console.log(checkFor1)
-//   })
-// } numberhood(11)
-
-
-
-
-
-
-// function numberhood(number) {
-//   let numberInput = [parseInt(number)];
-//   for (i = 0; i <= numberInput.length; i+=1) {
+// function number2(number) {
+//   let numberInput = number.toString()
+//   numberArray = [numberInput.split("")]
+//     for (i = 0; i < numberArray.length; i+=1) {
 //     if (numberInput.includes(1)) {
 //     return 'Beep!';
 //   } else {
 //     return numberInput
 //   }}
-// } numberhood(11)
+// } number2(111)
 
-
-
-
-// Trying to take input, split it into an array, and check whether array contains 1
-// function numberhood(number) {
-//   let numberInput = [parseInt(number)];
-//   let numberArray = numberInput.join(" ").split();
-//   console.log(numberArray)
-//   for (i = 0; i < numberArray.length; i+=1) {
-//     if (numberInput.includes(1)) {
-//     return 'Beep!';
+// function number3(number) {
+//   let numberInput = number.toString()
+//   numberArray = [numberInput.split("")]
+//     for (i = 0; i < numberArray.length; i+=1) {
+//     if (numberInput.includes(3)) {
+//     return "Won't you be my neighbor?";
 //   } else {
 //     return numberInput
 //   }}
-// }
+// } number3(111)
 
 
-// function numberhood(number) {
-//   let numberInput = [parseInt(number)];
-//   numberInput.toString().split('')
-//   for (i = 0; i <= numberInput.length; i++) {
-//     if (numberInput[i] === 1) {
-//     return 'Beep!';
-//   } else {
-//     return numberInput
-//   }}
-// } numberhood(11)
-
-
-
-// function numberhood(number) {
-//   let numberInput = [parseInt(number)];
-//   let checkfor1 = numberInput.includes(1);
-//   for (i = 0; i <= numberInput.length; i++) {
-//     if (checkfor1 === true) {
-//     return 'Beep!';
-//   } else { 
-//     return numberInput
-//   }}
-// } numberhood(11)
-
-
-
-// function numberhood(number) {
-//   let numberInput = [parseInt(number)];
-//   for (i = 0; i <= numberInput.length; i++) {
-//   if (numberInput[0] === 1) {
-//     return 'Beep!';
-//   } else { 
-//     return numberInput
-//   }}
-// } numberhood(11)
-
-// function numberhood(number) {
-//   let numberInput = [parseInt(number)];
-  
-  
-//   if (numberInput === 1) {
-//     return 'Beep!';
-//   } else {
-//     return numberInput
-//   }
-// } numberhood(1)
-
-
-// function numberhood(number) {
-//   let numberInput = parseInt(number);
-//   if (numberInput === 1) {
-//     return 'Beep!';
-//   } else {
-//     return numberInput
-//   }
-// } numberhood(1)
 
 //User Interface Logic
 
--- readme.md
+++ readme.md
@@ -1,51 +1,66 @@
-# Mr. Roboger's Neighborhood
+# Mr. Roboger's Numberhood
 ## by James Benjamin Pawlik (http://github.com/jbpawlik)
 
 ### __Description__
-Mr. Roboger's Neighborhood is an interactive website that takes numerical input from a user and returns numbers and strings depending on the number. For example, numbers that contain a 1 return "Beep!" while numbers with a 2 return "Boop!" Each subsequent rule has higher importance and overrides previous rules. 
+Mr. Roboger's Numberhood is an interactive website that takes numerical input from a user and returns numbers and strings depending on the number. For example, numbers that contain a 1 return "Beep!" while numbers with a 2 return "Boop!" Each subsequent rule has higher importance and overrides previous rules. 
 
 ### __Technologies Used__
-Mr. Roboger's Neighborhood uses custom HTML and CSS. JavaScript and JQuery are used for the business and interface logic.
+Mr. Roboger's Numberhood uses custom HTML and CSS. JavaScript and JQuery are used for the business and interface logic.
 
 ### __Setup/Installation__
 1. Download or clone the [repository](http://github.com/jbpawlik/numberhood) to your local machine
 2. Navigate to the top level of the directory
 3. Open index.html in your browser
+4. Enter a number in the number field to get a response
 
 Alternately, visit [Github Pages](http://jbpawlik.github.io/numberhood) to view the site.
 
 ### __Tests/Specifications__
 Describe numberhood()
 
-Test: "Should return the inputted number"
+Test 1: "Should return the user's input"
 Code:
   numberhood(21)
 Expected Output: "21" 
 
-Test: "If input is not a number, return NaN"
+Test 2: "If input is not a number, return NaN"
 Code:
   numberhood('a')
 Expected Output: NaN
 
-Test: "If number is 1, return 'Beep!'"
+Test 3: "If number is 1, return 'Beep!'"
 Code:
   numberhood(1)
 Expected Output: "Beep!"
 
-Test: "If number includes 1, return 'Beep!'"
+Test 4: "If number includes 1, return 'Beep!'"
 Code: 
   numberhood(21)
 Expected Output: "Beep!"
 
-Test: "If number includes 2, return 'Boop!'"
+Test 5: "If number is 2, return 'Boop!'"
+Code:
+  numberhood(2)
+Expected Output: "Boop!"
+
+Test 6: "If number includes 2, return 'Boop!"
+Code:
+  numberhood(12)
+Expected Output: "Boop!"
+
+Test 6: "If a number includes both a 1 and a 2, only return 'Boop!'"
 Code:
   numberhood(21)
-Expected Output: "Boop!"
+
+Test 7: "If number includes 3, return "Won't you be my neighbor?""
+Code:
+  numberhood(31)
+Expected Output: "Won't you be my neighbor?"
 
 ### __Known Bugs / Future Goals__
 No bugs have been found or reported. Please contact the author if you experience poor performance.
 
-Future goals for Mr. Roboger's Neighborhood are to take number, letter, and mixed data to return a wider variety of responses.
+Future goals for Mr. Roboger's Numberhood are to take alphanumberic and mixed data to return a wider variety of responses.
 
 ### __License__
 This software is licensed under the [BSD license](license.txt).
