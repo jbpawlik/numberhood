3ada339aac0036277f9c6b3c8e10873436732ff6
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-15 08:03:24 -0700

    WIP: rewrite scripts.js to output number sequence

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,48 @@
-//Business Logic
+//WIP: I misunderstood the prompt. The code that is not commented out is what I am working on for the resubmission. The commented-out code below it is a fully functional answer to the wrong problem. :)  My gh-pages site at jbpawlik.github.io/numberhood has the working original.
 
-function notANumber(number) {
-  if (isNaN(number) === false) {
-    return true;
-  } else {
-    return false;
+// Business Logic
+
+// function notANumber(number) {
+  function notANumber(number) {
+    if (isNaN(number) === true) {
+      return true;
+    } else {
+      return false;
+    }
   }
-} 
 
+//Loops need to: 
+//determine whether input number contains a 1, 2, or 3
+//output a count up from 0
+//replace 1, 2, or 3
+
+//forEach
 function numberhood(number) {
   let numberInput = number.toString();
   const numberArray = [numberInput.split("")];
-  if (notANumber(number) === true) {
-    for (i = 0; i < numberArray.length; i+=1) {
-      if (numberInput.includes(3)) {
-        return 'includes3';
-      } else if (numberInput.includes(2)) {
-        return 'includes2';
-      } else if (numberInput.includes(1)) {
-        return 'includes1';
-      } else return number;
-    }
-  }
-}
-
-function viralLoad(number) {
-  let virusArray = number.toString().split("");
-  return virusArray.map(digits => digits * 100).join(",") 
-}
+  let newArray = [0]
+  if (isNaN(number) === false) {
+    numberArray.forEach(function(index) {
+      newArray.push(index);
+      console.log(newArray.toString(", "))
+      })
+}} numberhood(111)
 
 
-//User Interface Logic
+//for loop
+function numberhood(number) {
+  let numberInput = number.toString();
+  const numberArray = [numberInput.split("")];
+  if (isNaN(number) === false) {
+    for (i = 0; i <= numberArray.length; i+=1) {
+      if (numberArray.includes(1)); {
+        numberArray.push(i);
+        return numberArray.toString();
+      }
+    }}
+} numberhood(0)
+
+// //User Interface Logic
 
 $(document).ready(function() {
   $('form#numberInput').submit(function(event) {
@@ -41,25 +53,16 @@ $(document).ready(function() {
   const userNumber = $("#userNumber").val();
   let result = userNumber
 
-  if (notANumber(userNumber) === false) {
+  if (notANumber(userNumber) === true) {
     $("#numberOutput").text('ERROR! ERROR! ERROR!  DOES NOT COMPUTE; USER DID NOT ENTER NUMBER; PRINT: "IT IS A TERRIBLE DAY IN THE NEIGHBORHOOD :("  WILL SELF DESTRUCT IF NO NUMBER ENTERED! 10... 9... 8...')
-  } else if (numberhood(userNumber) === 'includes3') {
-    $("#numberOutput").text(result + "!!! MY FAVORITE NUMBER!!! WON'T YOU BE MY NEIGHBOR???")
-  } else if (numberhood(userNumber) === 'includes2') {
-    $("#numberOutput").text(result + "!!! BOOP!!! BOOP!!! BOOP!!!")
-  } else if (numberhood(userNumber) === 'includes1') {
-    $("#numberOutput").text(result + "!!! BEEP! BEEP! BEEP!")
-  } else 
-    $("#numberOutput").text(result + "!!! GREAT NUMBER!")
-    $('#virusOutput').text(result)
-  });
-
+  }
+});
   $('button#virusButton').click(function(event) {
     event.preventDefault();
 
     const userNumber = $('#userNumber').val();
 
-    if (notANumber(userNumber) === false) {
+    if (notANumber(userNumber) === true) {
       alert('ERROR: NOT A NUMBER; VIRUS DEFEATED')
     } else {
     let virus = viralLoad(userNumber)
@@ -69,3 +72,76 @@ $(document).ready(function() {
     }
   });
 });
+
+// vvvvvvvvvv GREAT ANSWER TO THE WRONG QUESTION vvvvvvvvv
+
+//Business Logic
+
+// function notANumber(number) {
+//   if (isNaN(number) === true) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// } 
+
+// function numberhood(number) {
+//   let numberInput = number.toString();
+//   const numberArray = [numberInput.split("")];
+//   if (notANumber(number) === false) {
+//     for (i = 0; i < numberArray.length; i+=1) {
+//       if (numberInput.includes(3)) {
+//         return 'includes3';
+//       } else if (numberInput.includes(2)) {
+//         return 'includes2';
+//       } else if (numberInput.includes(1)) {
+//         return 'includes1';
+//       } else return number;
+//     }
+//   }
+// }
+
+// function viralLoad(number) {
+//   let virusArray = number.toString().split("");
+//   return virusArray.map(digits => digits * 100).join(",") 
+// }
+
+// //User Interface Logic
+
+// $(document).ready(function() {
+//   $('form#numberInput').submit(function(event) {
+//     event.preventDefault();
+    
+//   $("#outputField").show();
+
+//   const userNumber = $("#userNumber").val();
+//   let result = userNumber
+
+//   if (notANumber(userNumber) === true) {
+//     $("#numberOutput").text('ERROR! ERROR! ERROR!  DOES NOT COMPUTE; USER DID NOT ENTER NUMBER; PRINT: "IT IS A TERRIBLE DAY IN THE NEIGHBORHOOD :("  WILL SELF DESTRUCT IF NO NUMBER ENTERED! 10... 9... 8...')
+//   } else if (numberhood(userNumber) === 'includes3') {
+//     $("#numberOutput").text(result + "!!! MY FAVORITE NUMBER!!! WON'T YOU BE MY NEIGHBOR???")
+//   } else if (numberhood(userNumber) === 'includes2') {
+//     $("#numberOutput").text(result + "!!! BOOP!!! BOOP!!! BOOP!!!")
+//   } else if (numberhood(userNumber) === 'includes1') {
+//     $("#numberOutput").text(result + "!!! BEEP! BEEP! BEEP!")
+//   } else 
+//     $("#numberOutput").text(result + "!!! GREAT NUMBER!")
+//     $('#virusOutput').text(result)
+//   });
+
+//   $('button#virusButton').click(function(event) {
+//     event.preventDefault();
+
+//     const userNumber = $('#userNumber').val();
+
+//     if (notANumber(userNumber) === true) {
+//       alert('ERROR: NOT A NUMBER; VIRUS DEFEATED')
+//     } else {
+//     let virus = viralLoad(userNumber)
+//     alert('You won')
+//     alert(virus)
+//     alert("viruses!!!!!");
+//     }
+//   });
+// });
