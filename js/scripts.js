0a6fcc609dfa536f57e6cefb8e0015e383683d34
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-16 06:46:29 -0700

    generate array when numberhood called; WIP: not outputting array

--- js/scripts.js
+++ js/scripts.js
@@ -16,17 +16,73 @@
 //output a count up from 0
 //replace 1, 2, or 3
 
-//forEach
+
+//Counts to 10 (works)
+function count() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+}
+count();
+
+//Count to number?
+function countUp(number) {
+  const newArray = []
+  for (let i = 0; i <= number; i++) {
+    newArray.push(i);
+  }
+}
+countUp(10);
+
+
+function countToNumber(number) {
+  for (i = 1; i <= 10; i++)
+  console.log(number)
+} countToTen(10)
+
+
 function numberhood(number) {
-  let numberInput = number.toString();
-  const numberArray = [numberInput.split("")];
-  let newArray = [0]
+  let newArray = []
   if (isNaN(number) === false) {
-    numberArray.forEach(function(index) {
-      newArray.push(index);
-      console.log(newArray.toString(", "))
-      })
+    for (i = 0; i <= number; i++);
+    console.log(newArray.push(i));
+    return newArray;
 }} numberhood(111)
+
+//DOESN'T RETURN A USEABLE OUTPUT?
+function numberhood(number) {
+  let newArray = [];
+  for (i = 0; i <= number; i++) {
+    newArray.push(i);
+    return newArray;
+  }
+} numberhood(10)
+
+//WORKS - BUT WHY? AND HOW TO GET USABLE OUTPUT?
+//CAN'T return newArray?
+function numberhood(number) {
+  let newArray = [];
+  for (i = 0; i <= number; i++) {
+    console.log(newArray.push(i));
+    console.log(newArray);
+  }
+} numberhood(10)
+
+//HOW TO USE ARRAY OUTPUT IN FOREACH LOOP
+function changeNumber(number) {
+  let inputArray = [numberhood(number)];
+  inputArray.forEach(function(element) {
+    console.log(element)
+  })
+} changeNumber(10)
+
+
+//WHY DOES THIS WORK BUT THE ARRAY DOESN'T? I DON'T GET IT
+let array = [];
+for (let i = 1; i <= 10; i++) {
+   array.push(i);
+} array
+(10) [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 //for loop
