2333508b3fc58eb30d4ea0a689e989ac6cd0eef3
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-21 15:57:28 -0700

    refactor and fix identation

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,31 @@ function numberhood(number) {
   if (isNaN(number) === false) {
     for (i = 0; i <= number; i++) {
       newArray.push(i);
-    } return newArray;
+    } 
+    return newArray;
   }
 }
 
 //Returns array with 1s, 2s, and 3s changed
 function changeNumbers(number) {
-  let newArray = numberhood(number)
+  let newArray = numberhood(number);
   newArray.forEach(function(i) {
-    let elementString = i.toString()
+    let elementString = i.toString();
     if (elementString.includes(3) === true) {
-      newArray[i] = "Won't you be my neighbor?'"
+      newArray[i] = "Won't you be my neighbor?'";
     } else if (elementString.includes(2) === true) {
-      newArray[i] = 'Boop!'
+      newArray[i] = 'Boop!';
     } else if (elementString.includes(1) === true) {
-      newArray[i] = 'Beep!'
+      newArray[i] = 'Beep!';
     } 
   }) 
-  return newArray
+  return newArray;
 }
 
 //Calculates viral load if user clicks on spam link in email
 function viralLoad(number) {
   let virusArray = number.toString().split("");
-  return virusArray.map(digits => digits * 100).join(",") 
+  return virusArray.map(digits => digits * 100).join(",");
 }
 
 // //User Interface Logic
@@ -38,17 +39,16 @@ $(document).ready(function() {
   $('form#numberInput').submit(function(event) {
     event.preventDefault();
     
-  $("#outputField").show();
-  const userNumber = $("#userNumber").val();
-  
-  if (isNaN(userNumber) === true) {
-    $('#numberOutput').text('ERROR! ERROR! ERROR!  DOES NOT COMPUTE; USER DID NOT ENTER NUMBER; PRINT: "IT IS A TERRIBLE DAY IN THE NEIGHBORHOOD :("  WILL SELF DESTRUCT IF NO NUMBER ENTERED! 10... 9... 8...')
-    $('#virusOutput').text(userNumber)
-  } else {
-    $('#numberOutput').html("[" + changeNumbers(userNumber).join("] , [") + "]");
-    $('#virusOutput').text(userNumber)
-  }
-});
+    $("#outputField").show();
+    const userNumber = $("#userNumber").val();
+    
+    if (isNaN(userNumber) === true) {
+      $('#numberOutput').text('ERROR! ERROR! ERROR!  DOES NOT COMPUTE; USER DID NOT ENTER NUMBER; PRINT: "IT IS A TERRIBLE DAY IN THE NEIGHBORHOOD :("  WILL SELF DESTRUCT IF NO NUMBER ENTERED! 10... 9... 8...');
+    } else {
+      $('#numberOutput').html("[" + changeNumbers(userNumber).join("] , [") + "]");    
+    }
+    $('#virusOutput').text(userNumber);
+  });
 
   $('button#virusButton').click(function(event) {
     event.preventDefault();
@@ -56,12 +56,12 @@ $(document).ready(function() {
     const userNumber = $('#userNumber').val();
 
     if (isNaN(userNumber) === true) {
-      alert('ERROR: NOT A NUMBER; VIRUS DEFEATED')
+      alert('ERROR: NOT A NUMBER; VIRUS DEFEATED');
     } else {
-    let virus = viralLoad(userNumber)
-    alert('You won')
-    alert(virus)
-    alert("viruses!!!!!");
+      let virus = viralLoad(userNumber);
+      alert('You won');
+      alert(virus);
+      alert("viruses!!!!!");
     }
   });
 });
